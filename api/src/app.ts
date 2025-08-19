1b7af39d1308aeb517c372da71d5595d1fbc4836
agustinbravop <anbravoperez@gmail.com>
2025-08-18 23:35:34 -0300

    corregir configs

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,10 +2,4 @@ import { Hono } from "hono";
 
 export const app = new Hono();
 
-app.get("/api/time", (c) => {
-  // return local time in ISO format
-  const now = new Date();
-  return c.json({
-    time: now.toISOString(),
-  });
-});
+app.get("/api/time", (c) => c.json({ time: new Date().toISOString() }));
